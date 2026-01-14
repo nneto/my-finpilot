ec9f0da95e4baf6fc58dadee626a91d01fc5b2b9
Noberto Pessoa <7104467+nneto@users.noreply.github.com>
2026-01-14 15:06:43 -0300

    add app indicator gnome extension

--- build/build-gnome-extensions.sh
+++ build/build-gnome-extensions.sh
@@ -4,12 +4,18 @@ set -eoux pipefail
 
 echo "::group:: ===$(basename "$0")==="
 
+
+# Copy files
+ls -la /ctx/custom/gnome-shell/extensions/appindicatorsupport@rgcjonas.gmail.com
+rsync -rvK /ctx/custom/gnome-shell/extensions/ /usr/share/gnome-shell/extensions/
+
 # Install tooling
 dnf5 -y install glib2-devel meson sassc cmake dbus-devel
 
 # Build Extensions
 
 # AppIndicator Support
+ls -la /usr/share/gnome-shell/extensions/appindicatorsupport@rgcjonas.gmail.com
 glib-compile-schemas --strict /usr/share/gnome-shell/extensions/appindicatorsupport@rgcjonas.gmail.com/schemas
 
 rm /usr/share/glib-2.0/schemas/gschemas.compiled
